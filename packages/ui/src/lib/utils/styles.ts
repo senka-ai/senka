385018a37750d4a08e9121da31f7e66462fa5745
Catalin Ungureanu <catalinungureanu@gmail.com>
2025-07-23 02:10:41 +0300

    chore: text fields style update

--- packages/ui/src/lib/utils/styles.ts
+++ packages/ui/src/lib/utils/styles.ts
@@ -83,9 +83,9 @@ export const createButtonStyles = createStyleComposer({
 export const createInputStyles = createStyleComposer({
   base: 'w-full px-3.25 py-3.25 text-body-m text-neutral-900 bg-neutral-50 border rounded-xl transition-all duration-200 focus:outline-none focus:ring-offset-0 placeholder:text-neutral-500',
   variants: {
-    default: 'border-neutral-400 focus:border-highlight focus:ring-1 focus:ring-highlight-200',
+    default: 'border-neutral-400',
     focused: 'border-highlight ring-1 ring-highlight-400',
-    error: 'border-error-400 focus:border-error-400 focus:ring-1 focus:ring-error-400/20',
+    error: 'border-error-400 focus:ring-1 ring-error-400',
     disabled: 'border-neutral-300 bg-neutral-100 text-neutral-500 cursor-not-allowed',
   },
   sizes: {
